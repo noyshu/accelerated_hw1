9a0602ee937edd4e0128f70e57fd030a57a5a7d1
noyshu <noyshu@campus.technion.ac.il>
2018-06-05 17:02:10 +0300

    cuda check

--- hw1_after_sec4.cu
+++ hw1_after_sec4.cu
@@ -243,8 +243,8 @@ int main() {
             cudaMemcpyKind y = cudaMemcpyHostToDevice;
             int z = 1024 * sizeof(uchar);
             // TODO: copy relevant images from images1 and images2 to gpu_image1 and gpu_image2
-            CUDA_CHECK(cudaMemcpy(x, im1ptr, z, y));
-       //     CUDA_CHECK(cudaMemcpy(gpu_image2_shared+i*1024, im2ptr, 1024 * sizeof(uchar), cudaMemcpyHostToDevice));
+            //CUDA_CHECK(cudaMemcpy(x, im1ptr, z, y));
+            CUDA_CHECK(cudaMemcpy(gpu_image2_shared+i*1024, im2ptr, 1024 * sizeof(uchar), cudaMemcpyHostToDevice));
 
             image_to_hisogram_shared<<<1, threadsPerBlock>>>(gpu_image1_shared, gpu_hist1);
             image_to_hisogram_shared<<<1, threadsPerBlock>>>(gpu_image2_shared, gpu_hist2);
